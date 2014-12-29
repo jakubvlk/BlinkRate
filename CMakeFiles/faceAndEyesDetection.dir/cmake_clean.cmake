5c3973f524c3551c6d322a992979a06bfd2d5809
Jakub Vlk <kuba@craneballs.com>
2014-12-29 17:27:45 +0100

    init commit

---
+++ CMakeFiles/faceAndEyesDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/faceAndEyesDetection.dir/faceAndEyesDetection.cpp.o"
+  "faceAndEyesDetection.pdb"
+  "faceAndEyesDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/faceAndEyesDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
