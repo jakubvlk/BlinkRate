a0c3b0898f4561c6dd7bcf3795997d82f04c8262
Jakub Vlk <kuba@craneballs.com>
2015-01-02 14:09:37 +0100

    detekce oci na obrazku

--- faceAndEyesDetection.cpp
+++ faceAndEyesDetection.cpp
@@ -10,6 +10,7 @@ using namespace cv;
 
  /** Function Headers */
 void detectAndDisplay( Mat frame );
+void pupilDetection();//( Mat frame );
 
  /** Global variables */
 String face_cascade_name = "haarcascade_frontalface_alt.xml";
@@ -26,64 +27,130 @@ int main( int argc, const char** argv )
 	Mat frame;
 
    	//-- 1. Load the cascades
-   	if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-   	if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+   	if( !face_cascade.load( face_cascade_name ) )
+	{ 
+		printf("--(!)Error loading\n");
+		return -1; 
+	};
+	
+   	if( !eyes_cascade.load( eyes_cascade_name ) )
+	{ 
+		printf("--(!)Error loading\n"); return -1; 
+	};
 
 	//-- 2. Read the video stream
 	//capture =  cvCaptureFromCAM( -1 );
-		capture = cvCaptureFromFile("crazyManBlinks_480p.avi");
+	frame = imread("ja.jpg");// cvCaptureFromFile("crazyManBlinks_480p.avi");
 	
-	if( capture )
-	{
-		while( true )
-		{
-			frame = cvQueryFrame( capture );
-			
-			//-- 3. Apply the classifier to the frame
-			if( !frame.empty() )
-				{ detectAndDisplay( frame ); }
-			else
-				{ printf(" --(!) No captured frame -- Break!"); break; }
-
-			int c = waitKey(10);
-			if( (char)c == 'c' ) { break; }
-		}
-	}
+	// obrazek
+	detectAndDisplay(frame);
+	waitKey(0);
+	
+// if( capture )
+// 	{
+// 		while( true )
+// 		{
+// 			frame = cvQueryFrame( capture );
+//
+// 			//-- 3. Apply the classifier to the frame
+// 			if( !frame.empty() )
+// 			{
+// 				//detectAndDisplay( frame );
+// 				pupilDetection(frame);
+// 			}
+// 			else
+// 			{
+// 				printf(" --(!) No captured frame -- Break!"); break;
+// 			}
+//
+// 			int c = waitKey(10);
+// 			if( (char)c == 'c' )
+// 			{
+// 				break;
+// 			}
+// 		}
+// 	}
+	
+	//pupilDetection();
+	
 	return 0;
  }
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
-  std::vector<Rect> faces;
-  Mat frame_gray;
+	std::vector<Rect> faces;
+  	Mat frame_gray;
 
-  // convert from color to grayscale
-  cvtColor( frame, frame_gray, CV_BGR2GRAY );
-  // contrast adjustment using the image's histogram
-  equalizeHist( frame_gray, frame_gray );
+	// convert from color to grayscale
+	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	// contrast adjustment using the image's histogram
+  	equalizeHist( frame_gray, frame_gray );
 
-  //Detects objects (faces) of different sizes in the input image. The detected objects are returned as a list of rectangles.
-  face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+  	//Detects objects (faces) of different sizes in the input image. The detected objects are returned as a list of rectangles.
+  	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-  for( size_t i = 0; i < faces.size(); i++ )
-  {
-    Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+  	for( size_t i = 0; i < faces.size(); i++ )
+  	{
+    	Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+    	ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
 
-    Mat faceROI = frame_gray( faces[i] );
-    std::vector<Rect> eyes;
+    	Mat faceROI = frame_gray( faces[i] );
+    	std::vector<Rect> eyes;
 
-    //-- In each face, detect eyes
-    eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    	//-- In each face, detect eyes
+    	eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-    for( size_t j = 0; j < eyes.size(); j++ )
-     {
-       Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-       int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-       circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-     }
-  }
-  //-- Show what you got
-  imshow( window_name, frame );
- }
+    	for( size_t j = 0; j < eyes.size(); j++ )
+     	{
+       		Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
+       		int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+       		circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+     	}
+  	}
+  	//-- Show what you got
+  	imshow( window_name, frame );
+}
+
+void pupilDetection()//( Mat frame )
+{
+// Load image
+	cv::Mat src = cv::imread("ja.jpg");
+	// if (src.empty())
+// 		return -1;
+
+	// Invert the source image and convert to grayscale
+	cv::Mat gray;
+	cv::cvtColor(~src, gray, CV_BGR2GRAY);
+	cv::imshow("image1", gray);
+
+	// Convert to binary image by thresholding it
+	cv::threshold(gray, gray, 220, 255, cv::THRESH_BINARY);
+	cv::imshow("image2", gray);
+
+	// Find all contours
+	std::vector<std::vector<cv::Point> > contours;
+	cv::findContours(gray.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+	// Fill holes in each contour
+	cv::drawContours(gray, contours, -1, CV_RGB(255,255,255), -1);
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		double area = cv::contourArea(contours[i]);
+		cv::Rect rect = cv::boundingRect(contours[i]);
+		int radius = rect.width/2;
+
+		// If contour is big enough and has round shape
+		// Then it is the pupil
+		if (area >= 30 && 
+		    std::abs(1 - ((double)rect.width / (double)rect.height)) <= 0.2 &&
+				std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)	
+		{
+			cv::circle(src, cv::Point(rect.x + radius, rect.y + radius), radius, CV_RGB(255,0,0), 2);
+		}
+	}
+
+	cv::imshow("final image", src);
+	cv::waitKey(0);
+}
